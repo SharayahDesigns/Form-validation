d03f3dc2e934cf1aac315ec8f5af5540540bddf6
Sharayah Hefner <rocknrayah@gmail.com>
2022-09-07 17:00:08 -0600

    css styling

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const email = document.getElementById('email')
+const password2 = document.getElementById('password2')
+
